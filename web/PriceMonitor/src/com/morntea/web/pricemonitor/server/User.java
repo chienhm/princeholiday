77c27c42c1f6bfe66c111bf156b9d71e6caaf2d1
zyzhuyin <zyzhuyin@gmail.com>
2011-07-04 15:18:22 +0000

    Add the PriceMonitor project. Add the User class which is persistent.

---
+++ web/PriceMonitor/src/com/morntea/web/pricemonitor/server/User.java
@@ -0,0 +1,47 @@
+package com.morntea.web.pricemonitor.server;
+
+//import java.util.Date;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
+public class User {
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private long userId;
+
+    @Persistent
+    private String username;
+    
+    @Persistent
+    private String password;
+
+    /*
+    @Persistent
+    private String nickName;
+
+    @Persistent
+    private Date birthday;
+
+    @Persistent
+    private Date registerDate;
+    */
+    
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    
+    public long getId() {
+        return this.userId;
+    }
+    public String getUsername() {
+        return this.username;
+    }
+    public boolean isPassword(String password) {
+        return this.password.equals(password);
+    }
+}
